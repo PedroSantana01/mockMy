0a5e702bfbad3ad321c64829136af6dc9228c52f
PedroSantana01 <pedro.santana@fatec.sp.gov.br>
2018-03-26 18:36:22 -0300

    Create locus.py

---
+++ locus.py
@@ -0,0 +1,13 @@
+from locust import HttpLocust, TaskSet, task
+
+def index(l):
+	l.client.get("https://swarmlb.atlastest.tk")
+
+class UserBehavior(TaskSet):
+	tasks={index:2}
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 5000
+    max_wait = 9000
+
